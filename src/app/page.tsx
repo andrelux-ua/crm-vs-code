1e1106ccb98f6a63bbdbc746a4c80e82e9246a28
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2025-10-08 23:04:32 +0300

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import ActiveLabel from "./componets/active-label";
+import NotActiveLabel from "./componets/not-active-label";
+
 export default function Home() {
   return (
     <main>
       <h1>Home Page Project Next.js</h1>
+      <ActiveLabel>Active</ActiveLabel>
+      <NotActiveLabel>Not Active</NotActiveLabel>
     </main>
   );
 }
